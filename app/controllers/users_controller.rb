d56c279791de362e16408918484d6c29b2f576de
Vivek Harikrishnan <r.vivekharikrishnan@gmail.com>
2012-12-08 08:47:46 +0530

    index to show users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,10 @@
 class UsersController < ApplicationController
   def index
-            @professions = { "1" => "Accountant", "2" => "Advocate",
-    "3" => "Doctor", "4" => "Engineer", "5" => "Journalist", "6" => "Professor",
-    "7" => "Software Engineer","8" => "Teacher", "9" => "Others" }
+    @professions = { "1" => "Accountant", "2" => "Advocate",
+      "3" => "Doctor", "4" => "Engineer", "5" => "Journalist", "6" => "Professor",
+      "7" => "Software Engineer","8" => "Teacher", "9" => "Others" }
+
+    @users = User.all
   end
 
   def new
