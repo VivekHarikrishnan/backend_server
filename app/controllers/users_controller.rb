09e8790dab8228aa6c7f6415f3dcffc6eea8556a
Vivek Harikrishnan <r.vivekharikrishnan@gmail.com>
2012-12-08 08:59:25 +0530

    create user option is enabled

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,12 +8,18 @@ class UsersController < ApplicationController
   end
 
   def new
-        @professions = { "1" => "Accountant", "2" => "Advocate",
+    @professions = { "1" => "Accountant", "2" => "Advocate",
     "3" => "Doctor", "4" => "Engineer", "5" => "Journalist", "6" => "Professor",
     "7" => "Software Engineer","8" => "Teacher", "9" => "Others" }
     @user = User.new
   end
 
   def create
+    @user = User.new(params[:user])
+    if @user.save
+      redirect_to :action => :index
+    else
+      render :action => :new
+    end
   end
 end
